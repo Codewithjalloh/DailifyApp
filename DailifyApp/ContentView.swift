2eae11deecf57f0d304f7f1ef25ed19b84173765
codewithjalloh <codewithjalloh@gmail.com>
2024-07-11 21:21:25 +0100

    Initial Commit

---
+++ DailifyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailifyApp
+//
+//  Created by Lord Jalloh on 11/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailifyApp/DailifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailifyAppApp.swift
+//  DailifyApp
+//
+//  Created by Lord Jalloh on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DailifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
